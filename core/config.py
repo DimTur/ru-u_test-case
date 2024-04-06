020a44d12d4d5a0b36647810f37e8260b6a4a544
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-04-06 08:49:41 +0300

    init core and core/config

---
+++ core/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DbSettings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASS: str
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    @property
+    def url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+    echo: bool = False
+
+
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+    db: DbSettings = DbSettings()
+
+
+settings = Settings()
