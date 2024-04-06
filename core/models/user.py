759c02bb3846d8611184be893d8b050bb2448e4c
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-04-06 15:39:13 +0300

    reformat user model

--- core/models/user.py
+++ core/models/user.py
@@ -1,6 +1,24 @@
+import uuid
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
 from .base import Base
-from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 
 
-class User(SQLAlchemyBaseUserTableUUID, Base):
-    pass
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    name: Mapped[str] = mapped_column(String(32))
+    login: Mapped[str] = mapped_column(
+        String(32),
+        unique=True,
+        nullable=False,
+    )
+    hashed_password: Mapped[str] = mapped_column(String)
