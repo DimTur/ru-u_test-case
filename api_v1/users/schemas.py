5bd7def7f6bc85f16f4c75d7aacd783bc2e17a1d
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-04-06 15:48:30 +0300

    init api_v1 module users with schemas

---
+++ api_v1/users/schemas.py
@@ -0,0 +1,25 @@
+import uuid
+
+from typing import Annotated
+
+from annotated_types import MinLen, MaxLen
+from pydantic import BaseModel, ConfigDict
+
+
+class UserBase(BaseModel):
+    name: Annotated[str, MinLen(3), MaxLen(32)]
+    login: Annotated[str, MinLen(3), MaxLen(32)]
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdatePartial(UserCreate):
+    pass
+
+
+class User(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: uuid.UUID
