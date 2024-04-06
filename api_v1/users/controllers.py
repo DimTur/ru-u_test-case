701da24e141ae09578154b073569ade9c83f3212
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-04-06 19:09:37 +0300

    add auth

--- api_v1/users/controllers.py
+++ api_v1/users/controllers.py
@@ -4,7 +4,7 @@ from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.users.schemas import UserCreate
-from core.models import User
+from core.models import User, Token
 from secure import pwd_context
 
 
@@ -25,3 +25,17 @@ async def register_user_controller(session: AsyncSession, user_in: UserCreate) -
     await session.commit()
     await session.refresh(user)
     return user
+
+
+async def get_user_by_token(session: AsyncSession, access_token: str):
+    stmt = select(Token).where(Token.access_token == access_token)
+    result: Result = await session.execute(stmt)
+    token = result.scalar()
+
+    if token:
+        return await token.user
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="UNAUTHORIZED",
+        )
--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -1,9 +1,16 @@
+from typing import Annotated
+
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .controllers import register_user_controller
+from secure import apikey_scheme
+from .controllers import register_user_controller, get_user_by_token
 from .responses import post_user_responses
-from api_v1.users.schemas import User, UserCreate
+from api_v1.users.schemas import (
+    User,
+    UserCreate,
+    UserBase,
+)
 from core.models.db_helper import db_helper
 
 router = APIRouter(tags=["Users"])
@@ -23,4 +30,15 @@ async def register_user(
     return await register_user_controller(
         session=session,
         user_in=user_in,
+    )
+
+
+@router.get("/self/", response_model=UserBase)
+async def get_user_by_id(
+    access_token: Annotated[str, Depends(apikey_scheme)],
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await get_user_by_token(
+        session=session,
+        access_token=access_token,
     )
