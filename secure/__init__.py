19ea45d3044ce7808b22ca37c445f5d15c868067
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-04-07 12:10:40 +0300

    refactoring

--- secure/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi.security import APIKeyHeader
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto",
-)
-apikey_scheme = APIKeyHeader(name="Authorization")
