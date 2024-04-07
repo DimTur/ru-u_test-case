584754c8dc6d8423a675d6ded22d33d421bfc655
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-04-07 09:04:01 +0300

    add AuthJWT settings

--- core/config.py
+++ core/config.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 class DbSettings(BaseSettings):
@@ -18,6 +23,13 @@ class DbSettings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env")
     echo: bool = False  # True for debagging
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
 
 
 class Settings(BaseSettings):
